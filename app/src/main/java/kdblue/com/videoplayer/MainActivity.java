a858ed11157add2c887e57757a59a788ff4e0a7d
kuldeep mourya <kuldeep mourya>
2016-08-31 23:18:53 +0530

    Initial commit

---
+++ app/src/main/java/kdblue/com/videoplayer/MainActivity.java
@@ -0,0 +1,81 @@
+package kdblue.com.videoplayer;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+    ImageButton play;
+    int position = 0;
+    MediaController mediaController;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        if(mediaController==null)
+        {
+            mediaController=new MediaController(MainActivity.this);
+            mediaController.setAnchorView(videoView);
+            videoView.setMediaController(mediaController);
+        }
+
+        try {
+            // ID of video file.
+            int id = this.getRawResIdByName("minion");
+            videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + id));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    // Find ID corresponding to the name of the resource (in the directory raw).
+    public int getRawResIdByName(String resName) {
+        String pkgName = this.getPackageName();
+        // Return 0 if not found.
+        int resID = this.getResources().getIdentifier(resName, "raw", pkgName);
+        Log.i("AndroidVideoView", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get saved position.
+        position = savedInstanceState.getInt("CurrentPosition");
+        videoView.seekTo(position);
+    }
+
+    // When you change direction of phone, this method will be called.
+    // It store the state of video (Current position)
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        // Store current position.
+        savedInstanceState.putInt("CurrentPosition", videoView.getCurrentPosition());
+        videoView.pause();
+    }
+
+    public void videoPlay(View view)
+    {
+        String path= "android.resource://"+getPackageName()+"/"+R.raw.minion;
+
+        Uri uri= Uri.parse(path);
+        videoView.setVideoURI(uri);
+        videoView.start();
+    }
+
+}
